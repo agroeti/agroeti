6584ab4d7a0444698939392d3733f5ea8ce11fe8
Wlilian Mascimiano <willian@wsa.com.br>
2024-01-28 02:33:51 -0300

    feat: start project

---
+++ src/test/java/br/com/agroeti/AgroetiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agroeti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroetiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
